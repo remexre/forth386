87d911c9d72271ba58f4e75f7b10ee9ed37db865
Nathan Ringo <remexre@gmail.com>
2019-03-02 03:59:29 -0600

    Renames hexdump to hd.

--- src/forth/startup.f
+++ src/forth/startup.f
@@ -10,24 +10,24 @@
 : reasonable-taste $0f set-color ;
 : macos-user-or-summat $70 set-color ;
 
-: hexdump-write-nybble ( u -- ) $f and s" 0123456789abcdef" drop + c@ emit ;
-: hexdump-write-byte ( u -- )
+: hd-write-nybble ( u -- ) $f and s" 0123456789abcdef" drop + c@ emit ;
+: hd-write-byte ( u -- )
   dup $4 rshift
-  hexdump-write-nybble
-  hexdump-write-nybble ;
-: hexdump-write-dword ( u -- )
-  dup $18 rshift hexdump-write-byte
-  dup $10 rshift hexdump-write-byte
-  dup  $8 rshift hexdump-write-byte
-                 hexdump-write-byte ;
-: hexdump-write-row ( addr -- )
-  $b3 emit dup hexdump-write-dword $b3 emit
-  $10 0 do space dup i + c@ hexdump-write-byte loop
+  hd-write-nybble
+  hd-write-nybble ;
+: hd-write-dword ( u -- )
+  dup $18 rshift hd-write-byte
+  dup $10 rshift hd-write-byte
+  dup  $8 rshift hd-write-byte
+                 hd-write-byte ;
+: hd-write-row ( addr -- )
+  $b3 emit dup hd-write-dword $b3 emit
+  $10 0 do space dup i + c@ hd-write-byte loop
   space $b3 emit space
   $10 0 do dup i + c@ emit loop
   drop
   space $b3 emit cr ;
-: hexdump ( addr -- )
+: hd ( addr -- )
   \ BIG rip, it'd be nice to better syntax here...
   $da emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c2 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
@@ -40,9 +40,9 @@
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $bf emit cr
   $b3 emit space space space space space space space space $b3 emit
-  $10 0 do space space dup i + hexdump-write-nybble loop
+  $10 0 do space space dup i + hd-write-nybble loop
   space $b3 emit space
-  $10 0 do dup i + hexdump-write-nybble loop
+  $10 0 do dup i + hd-write-nybble loop
   space $b3 emit cr
   \ And another byte array here...
   $c3 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
@@ -55,7 +55,7 @@
   $c4 emit $c4 emit $c4 emit $c5 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $b4 emit cr
-  $80 0 do dup i + hexdump-write-row $10 +loop
+  $80 0 do dup i + hd-write-row $10 +loop
   drop
   \ More byte arrays being necessary...
   $c0 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
@@ -71,7 +71,7 @@
 
 \ : DOES> [ ' [DOES>] CFA ] LITERAL , ; IMMEDIATE
 \ : CONST CREATE , DOES> @ UNSMUDGE ;
-\ ' DOES> hexdump
+\ ' DOES> hd
 
 \ $123456 CONSTANT IPB
 \ ." IPB-CHECK" IPB @ $00425049 2DUP = . . . ;
