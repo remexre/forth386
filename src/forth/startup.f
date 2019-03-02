8258c46e85ed5763651c4a0da7432f148a3c4dd4
Nathan Ringo <remexre@gmail.com>
2019-03-02 03:56:00 -0600

    Rewrites hexdump to take advantage of loops.

--- src/forth/startup.f
+++ src/forth/startup.f
@@ -22,41 +22,10 @@
                  hexdump-write-byte ;
 : hexdump-write-row ( addr -- )
   $b3 emit dup hexdump-write-dword $b3 emit
-  \ This could benefit from a loop...
-  space dup      c@ hexdump-write-byte
-  space dup $1 + c@ hexdump-write-byte
-  space dup $2 + c@ hexdump-write-byte
-  space dup $3 + c@ hexdump-write-byte
-  space dup $4 + c@ hexdump-write-byte
-  space dup $5 + c@ hexdump-write-byte
-  space dup $6 + c@ hexdump-write-byte
-  space dup $7 + c@ hexdump-write-byte
-  space dup $8 + c@ hexdump-write-byte
-  space dup $9 + c@ hexdump-write-byte
-  space dup $a + c@ hexdump-write-byte
-  space dup $b + c@ hexdump-write-byte
-  space dup $c + c@ hexdump-write-byte
-  space dup $d + c@ hexdump-write-byte
-  space dup $e + c@ hexdump-write-byte
-  space dup $f + c@ hexdump-write-byte
+  $10 0 do space dup i + c@ hexdump-write-byte loop
   space $b3 emit space
-  \ This could benefit from a loop...
-  dup      c@ emit
-  dup $1 + c@ emit
-  dup $2 + c@ emit
-  dup $3 + c@ emit
-  dup $4 + c@ emit
-  dup $5 + c@ emit
-  dup $6 + c@ emit
-  dup $7 + c@ emit
-  dup $8 + c@ emit
-  dup $9 + c@ emit
-  dup $a + c@ emit
-  dup $b + c@ emit
-  dup $c + c@ emit
-  dup $d + c@ emit
-  dup $e + c@ emit
-      $f + c@ emit
+  $10 0 do dup i + c@ emit loop
+  drop
   space $b3 emit cr ;
 : hexdump ( addr -- )
   \ BIG rip, it'd be nice to better syntax here...
@@ -70,42 +39,10 @@
   $c4 emit $c4 emit $c4 emit $c2 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $bf emit cr
-  \ And a loop here...
   $b3 emit space space space space space space space space $b3 emit
-  space space dup      hexdump-write-nybble
-  space space dup $1 + hexdump-write-nybble 
-  space space dup $2 + hexdump-write-nybble
-  space space dup $3 + hexdump-write-nybble
-  space space dup $4 + hexdump-write-nybble
-  space space dup $5 + hexdump-write-nybble
-  space space dup $6 + hexdump-write-nybble
-  space space dup $7 + hexdump-write-nybble
-  space space dup $8 + hexdump-write-nybble
-  space space dup $9 + hexdump-write-nybble
-  space space dup $a + hexdump-write-nybble
-  space space dup $b + hexdump-write-nybble
-  space space dup $c + hexdump-write-nybble
-  space space dup $d + hexdump-write-nybble
-  space space dup $e + hexdump-write-nybble
-  space space dup $f + hexdump-write-nybble
-
+  $10 0 do space space dup i + hexdump-write-nybble loop
   space $b3 emit space
-  dup      hexdump-write-nybble
-  dup $1 + hexdump-write-nybble 
-  dup $2 + hexdump-write-nybble
-  dup $3 + hexdump-write-nybble
-  dup $4 + hexdump-write-nybble
-  dup $5 + hexdump-write-nybble
-  dup $6 + hexdump-write-nybble
-  dup $7 + hexdump-write-nybble
-  dup $8 + hexdump-write-nybble
-  dup $9 + hexdump-write-nybble
-  dup $a + hexdump-write-nybble
-  dup $b + hexdump-write-nybble
-  dup $c + hexdump-write-nybble
-  dup $d + hexdump-write-nybble
-  dup $e + hexdump-write-nybble
-  dup $f + hexdump-write-nybble
+  $10 0 do dup i + hexdump-write-nybble loop
   space $b3 emit cr
   \ And another byte array here...
   $c3 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
@@ -118,15 +55,8 @@
   $c4 emit $c4 emit $c4 emit $c5 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $b4 emit cr
-  \ This could benefit from a loop...
-  dup       hexdump-write-row
-  dup $10 + hexdump-write-row
-  dup $20 + hexdump-write-row
-  dup $30 + hexdump-write-row
-  dup $40 + hexdump-write-row
-  dup $50 + hexdump-write-row
-  dup $60 + hexdump-write-row
-      $70 + hexdump-write-row
+  $80 0 do dup i + hexdump-write-row $10 +loop
+  drop
   \ More byte arrays being necessary...
   $c0 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
   $c4 emit $c1 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit $c4 emit
