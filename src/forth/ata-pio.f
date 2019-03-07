9ac451c4404239a1c9f1fa4e94398861708ff39b
Nathan Ringo <remexre@gmail.com>
2019-03-07 15:49:06 -0600

    Finishes ATA-PIO driver.

--- src/forth/ata-pio.f
+++ src/forth/ata-pio.f
@@ -8,11 +8,13 @@
 
 \ Waits for the device to be ready.
 : ata-pio-wait-ready ( -- )
-  ata-pio-400ns-read $8 and 0= .s if recurse endif ;
+  ata-pio-400ns-read $8 and 0= if recurse endif ;
 
 \ Reads a sector into &ata-pio-buf[512*i].
 : ata-pio-read-sector ( i -- )
-  $100 do $1f0 inw i #2 pick #9 lshift or #1 lshift ata-pio-buf + w! loop ;
+  #512 * ata-pio-buf +
+  dup #512 + swap do $1f0 inw i w!
+  #2 +loop ;
 
 \ Sends a READ EXT for a given (48-bit) LBA.
 : ata-pio-send-read-ext ( lba-high-dword lba-low-dword -- )
@@ -24,14 +26,14 @@
        #8 rshift $1f5 outb \ Write byte 6
   $00 $1f2 outb \ Low byte of sector count.
   dup            $1f3 outb \ Write byte 1
-  dup  #8 rshift $1f3 outb \ Write byte 2
-      #16 rshift $1f3 outb \ Write byte 3
-  \ Send the "READ EXT" command.
-  $24 $1f7 outb ;
+  dup  #8 rshift $1f4 outb \ Write byte 2
+      #16 rshift $1f5 outb \ Write byte 3
+  $24 $1f7 outb ; \ Send the "READ EXT" command.
 
 \ Reads a megabyte off the disk starting at the given LBA.
 : ata-pio-read ( lba-high-dword lba-low-dword -- )
   ata-pio-send-read-ext
-  $800 times .s ata-pio-wait-ready .s i ata-pio-read-sector loop ;
+  $800 times ata-pio-wait-ready i ata-pio-read-sector loop ;
+0 0 ata-pio-read
 
 \ vim: set cc=80 ft=forth ss=2 sw=2 ts=2 et :
